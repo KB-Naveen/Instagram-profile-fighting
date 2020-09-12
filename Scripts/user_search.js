e88dd1337d69c51155beeed6126431ede9f9f2c4
kbnaveen123 <kbnaveen123@gmail.com>
2020-09-12 14:09:35 +0530

    added some css

--- Scripts/user_search.js
+++ Scripts/user_search.js
@@ -1,6 +1,6 @@
-document.getElementById("user1").addEventListener('keyup',userSearch(this.value));
+/*document.getElementById("user1").addEventListener('keyup',userSearch(this.value));
 document.getElementById("user2").addEventListener('keyup',userSearch(this.value));
 
 function userSearch(userName) {
     alert("You typed "+userName)
-}
+}*/
