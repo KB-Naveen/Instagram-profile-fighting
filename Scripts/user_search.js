e42be2863ead1d55a157a62e7c92ce549c76bcfd
kbnaveen123 <kbnaveen123@gmail.com>
2020-09-13 00:29:38 +0530

    for loop created

--- Scripts/user_search.js
+++ Scripts/user_search.js
@@ -4,4 +4,9 @@ document.getElementById('user1').addEventListener('keyup',async function start()
     var s_url = "https://www.instagram.com/web/search/topsearch/?context=user&query="+s_user
 
     fetch(s_url).then(response => response.json()).then(data => result=data);
+
+    var i;
+    for(i=0;i<=result.users.length;i++){
+        console.log(result.users[i].user.username)
+    }
 });
