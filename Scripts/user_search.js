58dee05806c804592f3431189c04e495900154a2
kbnaveen123 <kbnaveen123@gmail.com>
2020-09-13 14:33:17 +0530

    worked on username search results

--- Scripts/user_search.js
+++ Scripts/user_search.js
@@ -3,10 +3,41 @@ document.getElementById('user1').addEventListener('keyup',async function start()
     var s_user = this.value
     var s_url = "https://www.instagram.com/web/search/topsearch/?context=user&query="+s_user
 
-    fetch(s_url).then(response => response.json()).then(data => result=data);
+    var result
+    await fetch(s_url).then(response => response.json()).then(data => result=data);
 
-    var i;
-    for(i=0;i<=result.users.length;i++){
-        console.log(result.users[i].user.username)
+    var divtag = document.createElement('div')
+    var users = result.users;
+
+    if(users.length == 0){
+        var p_tag = document.createElement('p')
+        p_tag.innerHTML = 'User Not Found'
+        divtag.appendChild(ptag)
     }
+    else{
+        for(var user in users){
+            var a_tag = document.createElement('a')
+            a_tag.onclick = 'user_select(this)'
+            a_tag.value = users[user].user.username
+            var img_tag = document.createElement('img')
+            img_tag.src = users[user].user.profile_pic_url
+            var p_div_tag = document.createElement('div')
+            var p1 = document.createElement('p')
+            var p2 = document.createElement('p')
+            p1.innerHTML = users[user].user.username
+            p2.innerHTML = users[user].user.full_name
+            p1.className = 'username'
+            p2.className = 'full_name'
+
+
+            p_div_tag.appendChild(p1)
+            p_div_tag.appendChild(p2)
+            a_tag.appendChild(img_tag)
+            a_tag.appendChild(p_div_tag)
+            divtag.appendChild(a_tag)
+            
+        }
+    }
+    document.getElementById('search_result_block').innerHTML = divtag.innerHTML
+
 });
